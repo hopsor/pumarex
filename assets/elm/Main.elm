a76da9bbb2801c634cf29e138f724d3899a946bf
Victor Viruete <hopsor@gmail.com>
2017-03-02 00:53:40 +0100

    Added layout elements to the main elm module

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,7 +1,19 @@
 module Main exposing (..)
 
-import Html exposing (div, nav, ul, li, text)
+import Html exposing (div, header, nav, main_, ul, li, text)
 import Html.Attributes exposing (..)
 
 main =
-  div [ class "wrapper" ] []
+  div [ class "wrapper" ]
+  [ header [] []
+  , nav []
+    [ ul []
+      [ li [] [ text "Movies" ]
+      , li [] [ text "Rooms" ]
+      , li [] [ text "Screenings" ]
+      , li [] [ text "Box Office" ]
+      ]
+    ]
+  , main_ []
+    [ text "It works" ]
+  ]
