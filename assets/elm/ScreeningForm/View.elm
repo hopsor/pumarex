715dddc40c2427200e5b4479365812f08de89e81
Victor Viruete <hopsor@gmail.com>
2017-04-01 15:49:34 +0200

    Added content to New Screening form. Showing movies in the select box is using model.movieList record (not sure if it's the most suitable solution though)

--- assets/elm/ScreeningForm/View.elm
+++ assets/elm/ScreeningForm/View.elm
@@ -12,4 +12,53 @@ formView : Model -> Html Msg
 formView model =
     Html.form
         [ id "movie_form" ]
-        [ h1 [] [ text "New Screening" ] ]
+        [ h1 [] [ text "New Screening" ]
+        , div
+            [ class "field" ]
+            [ input
+                [ type_ "date", placeholder "Date" ]
+                []
+            , input
+                [ type_ "text", placeholder "HH:MM (24h format)" ]
+                []
+            ]
+        , div
+            [ class "field" ]
+            [ select
+                []
+                (movieOptions model)
+            ]
+        , div
+            [ class "field" ]
+            [ select
+                []
+                []
+            ]
+        , div
+            [ class "actions" ]
+            [ button
+                [ type_ "button", onClick Save ]
+                [ text "Save" ]
+            ]
+        ]
+
+
+movieOptions : Model -> List (Html Msg)
+movieOptions model =
+    case model.movieList of
+        Success result ->
+            let
+                movieOption : Movie -> Html Msg
+                movieOption =
+                    \movie ->
+                        option
+                            [ value (toString movie.id) ]
+                            [ text movie.title ]
+            in
+                List.map movieOption result.entries
+
+        _ ->
+            [ option
+                [ value "" ]
+                [ text "Not loaded" ]
+            ]
