0b1af254dfe52f5ed4f9fa661dced64a905e6118
Victor Viruete <hopsor@gmail.com>
2017-06-11 17:54:50 +0200

    updated mix dependencies

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Pumarex.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, github: "phoenixframework/phoenix", override: true},
+    [{:phoenix, "1.3.0-rc.2"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
