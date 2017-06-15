2351eaf563480ecc05b46caa20f035967849e246
Victor Viruete <hopsor@gmail.com>
2017-06-15 19:37:59 +0200

    forgot to add realm

--- lib/pumarex/web/router.ex
+++ lib/pumarex/web/router.ex
@@ -11,7 +11,7 @@ defmodule Pumarex.Web.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
   end
 
