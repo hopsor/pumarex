ebce7e8f8f50717cce51ada6de5b050f6a732def
Victor Viruete <hopsor@gmail.com>
2017-06-16 00:56:21 +0200

    moving http code to a helpers file

--- assets/elm/Helpers.elm
+++ assets/elm/Helpers.elm
@@ -3,6 +3,8 @@ module Helpers exposing (..)
 import Model exposing (Session)
 import Navigation
 import Routing exposing (parseLocation, Route(..))
+import Http
+import Json.Decode as Json
 
 
 getRoute : Maybe Session -> Navigation.Location -> Route
@@ -13,3 +15,29 @@ getRoute session location =
 
         Nothing ->
             NewSessionRoute
+
+
+getRequest : Session -> String -> Json.Decoder a -> Http.Request a
+getRequest session url decoder =
+    Http.request
+        { method = "GET"
+        , headers = [ Http.header "Authorization" ("Bearer " ++ session.jwt) ]
+        , url = url
+        , body = Http.emptyBody
+        , expect = Http.expectJson decoder
+        , timeout = Nothing
+        , withCredentials = True
+        }
+
+
+deleteRequest : Session -> String -> Http.Request String
+deleteRequest session url =
+    Http.request
+        { method = "DELETE"
+        , headers = [ Http.header "Authorization" ("Bearer " ++ session.jwt) ]
+        , url = url
+        , body = Http.emptyBody
+        , expect = Http.expectString
+        , timeout = Nothing
+        , withCredentials = True
+        }
--- assets/elm/MovieList/Commands.elm
+++ assets/elm/MovieList/Commands.elm
@@ -2,6 +2,7 @@ module MovieList.Commands exposing (..)
 
 import Decoders exposing (movieListDecoder)
 import Http
+import Helpers exposing (getRequest)
 import Model exposing (Session)
 import MovieList.Messages exposing (Msg(..))
 
@@ -9,18 +10,7 @@ import MovieList.Messages exposing (Msg(..))
 fetchMovies : Session -> Cmd Msg
 fetchMovies session =
     let
-        apiUrl =
-            "/api/movies"
-
         request =
-            Http.request
-                { method = "GET"
-                , headers = [ Http.header "Authorization" ("Bearer " ++ session.jwt) ]
-                , url = apiUrl
-                , body = Http.emptyBody
-                , expect = Http.expectJson movieListDecoder
-                , timeout = Nothing
-                , withCredentials = True
-                }
+            getRequest session "/api/movies" movieListDecoder
     in
         Http.send FetchMovies request
